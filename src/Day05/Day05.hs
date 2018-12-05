0576e0acadbd0c3e6d001310f7d4a9eee9b04fb8
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-05 13:21:25 +0000

    Day 5

---
+++ src/Day05/Day05.hs
@@ -0,0 +1,46 @@
+module Day05.Day05 where
+
+import Data.Char (isLower, isUpper, toLower)
+import Data.List (nub, sort)
+
+type Polymer = String
+
+
+getInput :: IO Polymer
+getInput = readFile "input.txt"
+
+
+-- Reduce a polymer by folding it
+-- https://en.wikipedia.org/wiki/Crystallization_of_polymers
+crystalise :: Polymer -> Polymer
+crystalise = foldr go ""
+  where
+    go :: Char -> Polymer -> Polymer
+    go c []     = [c]
+    go c (p:ps) = if reactive c p then ps else (c:p:ps)
+
+    reactive :: Char -> Char -> Bool
+    reactive a b = a /= b && toLower a == toLower b
+
+
+solution1 :: IO ()
+solution1 = do
+  result <- crystalise <$> getInput
+  putStrLn $ "Units remaining: " <> show (length result)
+
+
+solution2 :: IO ()
+solution2 = do
+  input <- getInput
+  let shortest
+        = head
+        . sort
+        . map (length . crystalise . removeUnit input)
+        . nub
+        . filter isUpper
+        $ input
+
+  putStrLn $ "Shortest chain: " <> show shortest
+  where
+    removeUnit :: Polymer -> Char -> Polymer
+    removeUnit p c = filter (/= toLower c) $ filter (/=c) p 
