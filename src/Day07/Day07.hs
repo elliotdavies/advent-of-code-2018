b2a25849c9359d45c9c06c83195f37ad95d0c5f5
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-08 14:01:16 +0000

    Finish day 7

--- src/Day07/Day07.hs
+++ src/Day07/Day07.hs
@@ -1,15 +1,11 @@
 module Day07.Day07 where
 
-
 import qualified Data.Map  as Map
-import           Data.List (isSubsequenceOf, sort, nub)
+import           Data.List (elemIndex, isSubsequenceOf, nub, partition, sort)
 
 
 data Dependency
   = Char `DependsOn` Char
-
-instance Show Dependency where
-  show (a `DependsOn` b) = [a] ++ " depends on " ++ [b]
 
 
 getInput :: IO [Dependency]
@@ -19,33 +15,63 @@ getInput = map (parse . words) <$> (lines <$> readFile "input.txt")
       = head step' `DependsOn` head step
 
 
+buildDependencyMap :: [Dependency] -> Map.Map Char [Char]
+buildDependencyMap deps
+  = foldr buildMap (initMap $ findAllSteps deps) deps
+  where
+    initMap = foldr (\c acc -> Map.insert c [] acc) Map.empty
+    
+    buildMap (a `DependsOn` b) = Map.adjust (b :) a
+
+    findAllSteps = nub . foldr (\(a `DependsOn` b) acc -> (a:b:acc)) ""
+
+
+candidateSteps :: [Char] -> Map.Map Char [Char] -> [Char]
+candidateSteps done 
+  = sort
+  . Map.keys
+  . Map.filterWithKey (\k deps -> deps `within` done && not (k `elem` done))
+  where
+    a `within` b = sort a `isSubsequenceOf` sort b
+
+
 solution1 :: IO ()
 solution1 = do
   deps <- getInput
-
-  let allSteps = nub . foldr (\(a `DependsOn` b) acc -> (a:b:acc)) "" $ deps
-      dependencyMap = foldr buildMap (initMap allSteps) deps
-      
-  putStrLn $ orderSteps Nothing "" dependencyMap
-
+  putStrLn $ orderSteps Nothing "" (buildDependencyMap deps)
   where
-    initMap = foldr (\c acc -> Map.insert c [] acc) Map.empty
-
-    buildMap (a `DependsOn` b) = Map.adjust (b :) a
-
-    a `within` b = sort a `isSubsequenceOf` sort b
-    
     orderSteps :: Maybe Char -> [Char] -> Map.Map Char [Char] -> [Char]
-    orderSteps maybeCurrent done map
-      = let done' = maybe done (\c -> done ++ [c]) maybeCurrent
-            candidates
-              = sort
-              . Map.keys
-              $ Map.filterWithKey
-                  (\k deps -> deps `within` done' && not (k `elem` done'))
-              map
+    orderSteps maybeCurrent done depMap
+      = let done'      = maybe done (\c -> done ++ [c]) maybeCurrent
+            candidates = candidateSteps done' depMap
 
         in  if length candidates == 0
               then done'
-              else orderSteps (Just $ head candidates) done' map
+              else orderSteps (Just $ head candidates) done' depMap
+
+
+solution2 :: IO ()
+solution2 = do
+  deps <- getInput
+  putStrLn . show $ orderSteps [] "" 0 (buildDependencyMap deps)
+  where
+    orderSteps :: [(Char, Int)] -> [Char] -> Int -> Map.Map Char [Char] -> Int
+    orderSteps current done count depMap
+      = let (newlyFinished, inProgress) = partition ((==1) . snd) current
+            
+            done' = done ++ map fst newlyFinished
+            
+            candidates
+              = map init
+              . take (5 - length inProgress)
+              . filter (\c -> not $ c `elem` (map fst inProgress))
+              $ candidateSteps done' depMap
+            
+            current' = map (\(c,t) -> (c,t-1)) inProgress ++ candidates
+
+        in  if length current' == 0
+              then count
+              else orderSteps current' done' (count + 1) depMap
+
+    init c = (c, maybe 0 (+61) (elemIndex c ['A'..'Z']))
 
