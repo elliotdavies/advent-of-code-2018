cc22abbf1e4b0b263f61dab1442a5457003de5ac
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-10 22:25:41 +0000

    Day 10

--- src/Day10/Day10.hs
+++ src/Day10/Day10.hs
@@ -1,8 +1,13 @@
 module Day10.Day10 where
 
+import Data.Char (isDigit)
+import Data.Foldable (for_)
+import qualified Data.Set as Set
+
 
 type Coord    = (Int, Int)
 type Velocity = (Int, Int)
+
 
 getInput :: IO [(Coord, Velocity)]
 getInput = do
@@ -10,9 +15,72 @@ getInput = do
   where
     parse :: String -> (Coord, Velocity)
     parse s
-      = let [_,x,y,_,vx,vy] = map (filter keep) . words $ s
-        in  ((read x, read y), (read vx, read vy))
+      = let [x,y,vx,vy] = map readInt . words . map keep $ s
+        in  ((x, y), (vx, vy))
       where
-        keep c = 
-        readInt = (read :: Int)
+        keep c = if isDigit c || c == '-' then c else ' '
 
+        readInt :: String -> Int
+        readInt = read
+
+
+step :: [(Coord, Velocity)] -> ([(Coord, Velocity)], Int, Int, Int, Int)
+step = foldr go ([], 0, 0, 0, 0)
+  where
+    go (cs, vs) (acc, minX, maxX, minY, maxY)
+      = let cs'@(x', y') = (fst cs + fst vs, snd cs + snd vs)
+        in ((cs', vs):acc, min minX x', max maxX x', min minY y', max maxY y')
+
+
+run :: [(Coord, Velocity)] -> Int -> Int -> ([(Coord, Velocity)], Int)
+run = go 0
+  where
+    go secs xs xLen yLen
+      = let (xs', minX, maxX, minY, maxY) = step xs
+            xLen' = maxX - minX
+            yLen' = maxY - minY
+        in  if xLen' > xLen && yLen' > yLen then (xs, secs) else go (secs+1) xs' xLen' yLen'
+
+
+printGrid :: [Coord] -> IO ()
+printGrid cs = do
+  let xs = map fst cs
+      ys = map snd cs
+      minX = minimum xs
+      maxX = maximum xs
+      minY = minimum ys
+      maxY = maximum ys
+
+      set = Set.fromList cs
+  
+  for_ [minY..maxY] $ \y -> do
+    for_ [minX..maxX] $ \x -> printLine x y set
+    putStrLn ""
+
+  where
+    printLine :: Int -> Int -> Set.Set Coord -> IO ()
+    printLine x y cs
+      = putStr $ if Set.member (x,y) cs then "#" else "."
+
+
+getResult :: IO ([Coord], Int)
+getResult = do
+  input <- getInput
+
+  let cs = map fst input
+      xs = map fst cs
+      ys = map snd cs
+      minX = minimum xs
+      maxX = maximum xs
+      minY = minimum ys
+      maxY = maximum ys
+
+  let (csvs, t) = run input (maxX - minX) (maxY - minY)
+  pure (map fst csvs, t)
+
+
+solution1 :: IO ()
+solution1 = getResult >>= printGrid . fst
+
+solution2 :: IO ()
+solution2 = getResult >>= putStrLn . show . snd
