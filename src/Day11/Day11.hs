7fead8865016b256ab8f34fa9670da67272edba4
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-11 09:07:44 +0000

    Day 11 equations

--- src/Day11/Day11.hs
+++ src/Day11/Day11.hs
@@ -1,1 +1,22 @@
 module Day11.Day11 where
+
+newtype Coord = Coord (Int, Int)
+
+
+input :: Int
+input = 8
+-- input = 8772
+
+
+cellPower :: Coord -> Int
+cellPower (Coord (x,y))
+  = let rackId = x + 10
+    in  hundreds ((y * rackId + input) * rackId) - 5
+
+
+hundreds :: Int -> Int
+hundreds i
+  = case drop 2 . take 3 . reverse . show $ i of
+      [] -> 0
+      x  -> read x :: Int
+
