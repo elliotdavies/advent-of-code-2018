ab62391f3e43b7447831a336fc85c2e24a403323
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-11 12:58:32 +0000

    Day 11 pt 1

--- src/Day11/Day11.hs
+++ src/Day11/Day11.hs
@@ -1,11 +1,25 @@
 module Day11.Day11 where
 
+import Data.List (maximumBy)
+
+
 newtype Coord = Coord (Int, Int)
+  deriving (Show)
+
+
+data Res = Res Coord Int
+  deriving (Show)
+
+instance Eq Res where
+  (Res _ a) == (Res _ b) = a == b
+
+instance Ord Res where
+  (Res _ a) `compare` (Res _ b) = a `compare` b
 
 
 input :: Int
-input = 8
--- input = 8772
+input = 18
+--input = 8772
 
 
 cellPower :: Coord -> Int
@@ -20,3 +34,31 @@ hundreds i
       [] -> 0
       x  -> read x :: Int
 
+
+genGrid :: Int -> [Coord]
+genGrid size =
+  Coord <$> [(x,y) | x <- [0 .. size - 3], y <- [0 .. size - 3]]
+
+
+gridSquarePower :: Coord -> Res
+gridSquarePower (Coord (x,y))  
+  = let total = sum . map cellPower $
+             [ Coord (x,y), Coord (x+1,y), Coord (x+2,y)
+             , Coord (x,y+1), Coord (x+1,y+1), Coord (x+2,y+1)
+             , Coord (x,y+2), Coord (x+1,y+2), Coord (x+2,y+2)
+             ]
+    in  Res (Coord (x,y)) total
+
+
+solution1 :: IO ()
+solution1 = do
+  let grid = genGrid 300
+      res  = maximum $ map gridSquarePower grid
+  putStrLn $ show res
+
+
+solution2 :: IO ()
+solution2 = do
+  let grids = map genGrid [3..300]
+      res   = maximum . map (maximum . map gridSquarePower) $ grids
+  putStrLn $ show res
