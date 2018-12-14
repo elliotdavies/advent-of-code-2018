6a22a86a857878227220e8b80b4b8520735809bb
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-14 18:59:19 +0000

    Day 13 pt 1 working for sample code

--- src/Day13/Day13.hs
+++ src/Day13/Day13.hs
@@ -1,7 +1,7 @@
 module Day13.Day13 where
 
 import           Control.Arrow   (first, second)
-import           Data.List.Index (ifoldr)
+import           Data.List       (sort)
 import           Data.List.Index (ifoldr)
 import qualified Data.Map        as Map
 import           Data.Maybe      (fromJust)
@@ -10,9 +10,9 @@ import           Data.Maybe      (fromJust)
 type Pos = (Int, Int)
 
 incX = first (+1)
-decX = first ((-)1)
+decX = first (subtract 1)
 incY = second (+1)
-decY = second ((-)1)
+decY = second (subtract 1)
 
 
 data Direction
@@ -20,6 +20,13 @@ data Direction
   | D
   | L
   | R
+  deriving (Show)
+
+
+data Turn
+  = TLeft
+  | TStraight
+  | TRight
   deriving (Show)
 
 
@@ -37,8 +44,14 @@ type Tracks
 
 
 data Cart
-  = Cart Direction (Int, Int)
+  = Cart Direction Turn (Int, Int)
   deriving (Show)
+
+instance Eq Cart where
+  (Cart _ _ a) == (Cart _ _ b) = a == b
+
+instance Ord Cart where
+  (Cart _ _ a) `compare` (Cart _ _ b) = a `compare` b
 
 
 getInput :: IO (Tracks, [Cart])
@@ -62,46 +75,75 @@ getInput = do
           _    -> (tracks, carts)
       where
         ins t = Map.insert (x,y) t tracks
-        app d t = (ins t, Cart d (x,y) : carts)
+        app d t = (ins t, Cart d TLeft (x,y) : carts)
 
 
 step :: Tracks -> [Cart] -> [Cart]
 step tracks
   = foldr step' []
   where
-    step' (Cart d pos) acc
+    step' (Cart d turn pos) acc
       = let currentTrack = fromJust $ Map.lookup pos tracks
-            (d', pos') = getNext currentTrack d pos
-        in  Cart d' pos' : acc
+            (d', turn', pos') = getNext currentTrack d turn pos
+        in  Cart d' turn' pos' : acc
 
-    getNext t d pos
-      = case (t, d) of
-          (Vertical, U)     -> (U, decY pos)
-          (Vertical, D)     -> (U, incY pos)
-          (Horizontal, L)   -> (L, decX pos)
-          (Horizontal, R)   -> (L, incX pos)
-          (CurveBSlash, R)  -> (D, incY pos)
-          (CurveBSlash, U)  -> (L, decX pos)
-          (CurveFSlash, L)  -> (D, incY pos)
-          (CurveFSlash, U)  -> (R, incX pos)
-          (Intersection, _) -> (d, pos) -- TODO
-          _                 -> (d, pos)
+    getNext track d turn pos
+      = case (track, d) of
+          (Vertical, U)     -> (U, turn, decY pos)
+          (Vertical, D)     -> (D, turn, incY pos)
+          (Horizontal, L)   -> (L, turn, decX pos)
+          (Horizontal, R)   -> (R, turn, incX pos)
+          (CurveBSlash, R)  -> (D, turn, incY pos)
+          (CurveBSlash, U)  -> (L, turn, decX pos)
+          (CurveBSlash, L)  -> (U, turn, decY pos)
+          (CurveBSlash, D)  -> (R, turn, incX pos)
+          (CurveFSlash, L)  -> (D, turn, incY pos)
+          (CurveFSlash, U)  -> (R, turn, incX pos)
+          (CurveFSlash, R)  -> (U, turn, decY pos)
+          (CurveFSlash, D)  -> (L, turn, decX pos)
+          (Intersection, _) -> let (turn', d', pos') = handleIntersection turn d pos in (d', turn', pos')
+          _                 -> (d, turn, pos)
+
+    handleIntersection turn d pos
+      = case (turn, d) of
+          (TLeft, U) -> (TStraight, L, decX pos)
+          (TLeft, R) -> (TStraight, U, decY pos)
+          (TLeft, D) -> (TStraight, R, incX pos)
+          (TLeft, L) -> (TStraight, D, incY pos)
+          (TStraight, U) -> (TRight, U, decY pos)
+          (TStraight, R) -> (TRight, R, incX pos)
+          (TStraight, D) -> (TRight, D, incY pos)
+          (TStraight, L) -> (TRight, L, decX pos)
+          (TRight, U) -> (TLeft, R, incX pos)
+          (TRight, R) -> (TLeft, D, incY pos)
+          (TRight, D) -> (TLeft, L, decX pos)
+          (TRight, L) -> (TLeft, U, decY pos)
 
 
 
-collision :: [Cart] -> Bool
-collision = foldl matches False . sort
+collision :: [Cart] -> Maybe (Int, Int)
+collision = go . sort
   where
-    matches res []       = res
-    matches res [x]      = res
-    matches res (x:y:xs) = res && x == y
+    go (x:y:xs) = if x == y then let Cart _ _ pos = x in Just pos else go (y:xs)
+    go _        = Nothing
+
+
+
+run :: Tracks -> [Cart] -> (Int, Int)
+run tracks carts
+  = go carts
+  where
+    go cs
+      = let cs' = step tracks cs
+        in  case collision cs' of
+              Just pos -> pos
+              Nothing  -> go cs'
+
 
 
 solution1 :: IO ()
 solution1 = do
   (tracks, carts) <- getInput
-  putStrLn $ show carts
-
-  let carts' = step tracks carts
-  putStrLn $ show carts'
+  let collisionPos = run tracks carts
+  putStrLn $ show collisionPos
 
