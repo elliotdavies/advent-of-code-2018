a6a10b3ffd58da1ebe4f62f587c8040940a2ac1b
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-20 17:09:23 +0000

    Day 19 pt 1

---
+++ src/Day19/Day19.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Day19.Day19 where
+
+import            Day16.Day16   (Registers, OpCode(..))
+import qualified  Day16.Day16   as D16
+import qualified  Data.IntMap   as M
+import qualified  Data.Vector   as V
+
+
+data State
+  = State Int Int Registers -- register ip is bound to; value of ip
+  deriving (Show)
+
+
+data Instruction
+  = Instruction OpCode Int Int Int
+  deriving (Show)
+
+
+type Instructions = V.Vector Instruction
+
+
+initRegisters :: Registers
+initRegisters = M.fromList [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)]
+
+
+getInput :: Registers -> IO (State, Instructions)
+getInput rs = do
+  (ip:instrs) <- lines <$> readFile "input.txt"
+  let state = State (read $ words ip !! 1) 0 rs
+      instructions = V.fromList $ map parseInstr instrs
+  
+  pure (state, instructions)
+  where
+    parseInstr s
+      = let [code,a,b,c] = words s
+        in  Instruction (parseOpCode code) (read a) (read b) (read c)
+    
+    parseOpCode = \case
+      "addr" -> AddR
+      "addi" -> AddI
+      "mulr" -> MulR
+      "muli" -> MulI
+      "banr" -> BAnR
+      "bani" -> BAnI
+      "borr" -> BOrR
+      "bori" -> BOrI
+      "setr" -> SetR
+      "seti" -> SetI
+      "gtir" -> GtiR
+      "gtri" -> GtrI
+      "gtrr" -> GtrR
+      "eqir" -> EqiR
+      "eqri" -> EqrI
+      "eqrr" -> EqrR
+
+
+-- @TODO Refactor Day 16 so that the code reuse is nicer
+execute :: Instruction -> State -> State
+execute (Instruction code a b c) (State ipReg ipVal rs)
+  = let rs' = D16.execute (D16.Instruction 0 a b c) code (M.insert ipReg ipVal rs)
+        ipVal' = (rs' M.! ipReg) + 1
+    in  State ipReg ipVal' rs'
+
+
+runProgram :: Instructions -> State -> State
+runProgram instrs state@(State _ ipVal _)
+  = case flip execute state <$> instrs V.!? ipVal of
+      Just state' -> runProgram instrs state'
+      Nothing     -> state
+
+
+solution1 :: IO ()
+solution1 = do
+  (state, instrs) <- getInput initRegisters
+  let State _ _ rs = runProgram instrs state
+  print $ rs M.! 0
+
+
+-- solution2 :: IO ()
+-- solution2 = do
+--   (state, instrs) <- getInput $ M.insert 0 1 initRegisters
+--   let State _ _ rs = runProgram instrs state
+--   print $ rs M.! 0
