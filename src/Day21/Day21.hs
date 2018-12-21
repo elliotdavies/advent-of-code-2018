cb6b88fa5c06e8c98050f16dd8a0b3c4a460cae4
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-21 11:03:54 +0000

    Simplify

--- src/Day21/Day21.hs
+++ src/Day21/Day21.hs
@@ -8,23 +8,20 @@ import qualified  Data.IntMap as      M
 import qualified  Data.Vector as      V
 
 
-runProgram :: Instructions -> State -> (State, Int)
-runProgram instrs state
-  = go instrs (state, 0)
-  where
-    go instrs (state@(State _ ipVal _), count)
-      -- Line 30 is the first time register 0 is actually used, for an
-      -- `eqrr 4 0 2`. So we need to look at register 4 at that point
-      -- to tell us what value of register 0 will cause termination
-      = if ipVal == 30 then (state, count)
-        else
-          case flip execute state <$> instrs V.!? ipVal of
-            Just state' -> go instrs (state', count + 1)
-            Nothing     -> (state, count)
+runProgram :: Instructions -> State -> State
+runProgram instrs state@(State _ ipVal _)
+  -- Line 30 is the first time register 0 is actually used, for
+  -- `eqrr 4 0 2`. So we can look at register 4 at that point
+  -- to tell us what value of register 0 will cause termination
+  = if ipVal == 30
+      then  state
+      else  case flip execute state <$> instrs V.!? ipVal of
+              Just state' -> runProgram instrs state'
+              Nothing     -> state
 
 
 solution1 :: IO ()
 solution1 = do
   (state, instrs) <- getInput initRegisters
-  let (State _ _ rs, count) = runProgram instrs state
+  let State _ _ rs = runProgram instrs state
   putStrLn $ "Register 4: " ++ show (rs M.! 4)
