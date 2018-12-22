b58c81a9eba054408a5e458ab40b1995c255c0fd
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-22 13:19:29 +0000

    Start day 22

---
+++ src/Day22/Day22.hs
@@ -0,0 +1,69 @@
+module Day22.Day22 where
+
+import           Data.List (sort)
+import qualified Data.Map        as M
+import           Linear          (V2(..))
+
+
+newtype Pos = Pos (V2 Int)
+  deriving (Show, Eq)
+
+instance Ord Pos where
+  Pos (V2 x y) `compare` Pos (V2 x' y')
+    = y `compare` y' <> x `compare` x'
+
+
+data RegionType
+  = Rocky
+  | Wet
+  | Narrow
+  deriving (Enum, Show)
+
+
+type Grid = M.Map Pos (Int, Int, RegionType)
+
+
+depth :: Int
+depth = 510
+
+
+target :: Pos
+target = Pos $ V2 10 10
+
+
+genGrid :: Grid
+genGrid
+  = let Pos (V2 targetX targetY) = target
+        ps = Pos <$>
+              [ V2 x y
+              | x <- [0 .. targetX]
+              , y <- [0 .. targetY]
+              ]
+    in foldl makeRegion M.empty (sort ps)
+  where
+    makeRegion :: Grid -> Pos -> Grid
+    makeRegion g pos
+      = let idx = geologicIndex g pos
+            el  = erosionLevel idx
+            rt  = regionType el
+          in  M.insert pos (idx, el, rt) g
+
+
+geologicIndex :: Grid -> Pos -> Int
+geologicIndex g pos@(Pos (V2 x y))
+  | x == 0 && y == 0  = 0
+  | pos == target     = 0
+  | y == 0            = x * 16807
+  | x == 0            = y * 48271
+  | otherwise =
+      let (_, el1, _) = g M.! (Pos (V2 (x-1) y))
+          (_, el2, _) = g M.! (Pos (V2 x (y-1)))
+      in  el1 * el2
+
+
+erosionLevel :: Int -> Int
+erosionLevel idx = (idx + depth) `mod` 20183
+
+
+regionType :: Int -> RegionType
+regionType el = [Rocky ..] !! (el `mod` 3)
