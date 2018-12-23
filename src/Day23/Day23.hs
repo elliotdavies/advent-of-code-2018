308028813c509eb1af4602b3e7868e3bb6bbc3c9
Elliot Davies <elliot.a.davies@gmail.com>
2018-12-23 12:46:41 +0000

    Thinking about day 23 pt 2

--- src/Day23/Day23.hs
+++ src/Day23/Day23.hs
@@ -1,7 +1,7 @@
 module Day23.Day23 where
 
 import Data.Char (isDigit)
-import Data.List (sortOn)
+import Data.List (foldl', sortOn)
 import Linear (V3(..))
 
 
@@ -9,7 +9,7 @@ type Pos = V3 Int
 
 
 data Bot
-  = Bot Pos Int
+  = Bot { _pos :: Pos, _radius :: Int }
   deriving (Eq, Show)
 
 
@@ -29,9 +29,7 @@ getInput = do
 
 
 widestSignal :: [Bot] -> Bot
-widestSignal = head . reverse . sortOn radius
-  where
-    radius (Bot _ r) = r
+widestSignal = head . reverse . sortOn _radius
 
 
 numInRange :: [Bot] -> Bot -> Int
@@ -53,3 +51,37 @@ solution1 = do
   bots <- getInput
   let strongest = widestSignal bots
   print $ numInRange bots strongest
+
+
+-- @TODO Find a way to optimise this
+solution2 :: IO ()
+solution2 = do
+  bots <- getInput
+ 
+  let ps = map _pos bots
+      xs = map (\(V3 x _ _) -> x) ps 
+      ys = map (\(V3 _ y _) -> y) ps 
+      zs = map (\(V3 _ _ z) -> z) ps 
+      coordsToTry = [ V3 x y z
+                    | x <- [ minimum xs .. maximum xs ]
+                    , y <- [ minimum ys .. maximum ys ]
+                    , z <- [ minimum zs .. maximum zs ]
+                    ]
+
+  let origin = V3 0 0 0
+      bestPos = snd $ foldl' (step bots) (0, origin) coordsToTry
+  
+  print bestPos
+  print $ manhattanDistance bestPos origin 
+  where
+    step bots acc@(bestNumInRange, bestPos) pos
+      = let n = numCanReach pos bots
+        in  if n > bestNumInRange then (n, pos) else acc
+
+
+numCanReach :: Pos -> [Bot] -> Int
+numCanReach pos
+  = length . filter (canReach pos)
+  where
+    canReach pos bot
+      = manhattanDistance (_pos bot) pos <= _radius bot
